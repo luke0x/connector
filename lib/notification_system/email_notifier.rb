53c786c3f5d357aa6421cd6fe94a3f5a46aa7090
scott@joyent.com <scott@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2007-08-07 23:52:16 +0000

    fix some issues with emailnotifier
    git-svn-id: http://svn.joyent.com/opensource/connector/source/trunk@272 7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a

--- lib/notification_system/email_notifier.rb
+++ lib/notification_system/email_notifier.rb
@@ -1,15 +1,16 @@
 module NotificationSystem
   class EmailNotifier
-    def self.notify(notifaction)
+    def self.notify(notification)
       host          = ENV['SMTP_HOST'] || JoyentConfig.smtp_host
       user          = ENV['SMTP_USER'] || nil
       pass          = ENV['SMTP_PASS'] || nil
       auth          = ENV['SMTP_AUTH'] || nil
 
       tmail         = TMail::Mail.new
-      tmail.body    = "You are being notified of #{notification.name} (#{notification.item.class_humanize}) by #{notification.notifier.full_name}."
-      tmail.to      = notification.notifiee.system_email
-      tmail.subject = "Connector Notification: #{notification.name}"
+      tmail.body    = "You are being notified of #{notification.item.name} (#{notification.item.class_humanize}) by #{notification.notifier.full_name}."
+      tmail.to      = notification.notifiee.system_email # TODO needs to change to stored preference, which isn't done yet
+      tmail.from    = notification.notifier.system_email
+      tmail.subject = "Connector Notification: #{notification.item.name}"
 
       unless ENV['RAILS_ENV'] == 'test'
         Net::SMTP.start(host, 25, host, user, pass, auth) do |smtp|
