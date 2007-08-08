932d142a5e406f1b74bff0f7be0e868024ed8dd1
scott@joyent.com <scott@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2007-08-08 18:08:57 +0000

    Here's how we might handle event alarms
    git-svn-id: http://svn.joyent.com/opensource/connector/source/trunk@286 7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a

--- lib/notification_system/email_notifier.rb
+++ lib/notification_system/email_notifier.rb
@@ -1,23 +1,37 @@
 module NotificationSystem
   class EmailNotifier
     def self.notify(notification)
-      host          = ENV['SMTP_HOST'] || JoyentConfig.smtp_host
-      user          = ENV['SMTP_USER'] || nil
-      pass          = ENV['SMTP_PASS'] || nil
-      auth          = ENV['SMTP_AUTH'] || nil
-
       body =<<EOS
 #{notification.notifier.full_name} notified you about #{notification.item.name} (#{notification.item.class_humanize}).
 
 #{MessageHelper.url_for(notification.item)}
 EOS
 
-      tmail         = TMail::Mail.new
-      tmail.to      = notification.notifiee.system_email # TODO needs to change to stored preference, which isn't done yet
-      tmail.from    = notification.notifier.system_email
-      tmail.subject = "Connector Notification: #{notification.item.name}"
-      tmail.body    = body
+      send_message :subject => "Connector Notification: #{notification.item.name}", 
+                   :body    => body,
+                   :to      => notification.notifiee.system_email, # TODO needs to change to stored pref
+                   :from    => notification.notifier.system_email
+    end
+    
+    def self.alarm(event)
+      send_message :subject => "Connector Alarm: #{event.name}",
+                   :body    => "#{event.start_time.strftime('%D %T')}\n#{event.name}\n#{event.location}",
+                   :to      => event.owner.system_email, # TODO needs to change to stored pref
+                   :from    => event.owner.system_email
+    end
+    
+    def self.send_message(opts)
+      host          = ENV['SMTP_HOST'] || JoyentConfig.smtp_host
+      user          = ENV['SMTP_USER'] || nil
+      pass          = ENV['SMTP_PASS'] || nil
+      auth          = ENV['SMTP_AUTH'] || nil
 
+      tmail         = TMail::Mail.new
+      tmail.to      = opts[:to]
+      tmail.from    = opts[:from]
+      tmail.subject = opts[:subject]
+      tmail.body    = opts[:body]
+      
       unless ENV['RAILS_ENV'] == 'test'
         Net::SMTP.start(host, 25, host, user, pass, auth) do |smtp|
           smtp.send_message tmail.encoded, tmail.from, tmail.destinations
