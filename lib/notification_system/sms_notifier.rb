f49d88db0cfe29fd9af8ad54a9dd0bfef2c6a246
scott.burton@joyent.com <scott.burton@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2007-08-09 18:36:38 +0000

    - Ability to have multiple SMS recipients - Moved url to notification item to the top of the message in-case it gets truncated by the provider
    git-svn-id: http://svn.joyent.com/opensource/connector/source/trunk@309 7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a

--- lib/notification_system/sms_notifier.rb
+++ lib/notification_system/sms_notifier.rb
@@ -9,30 +9,29 @@
 module NotificationSystem
   class SmsNotifier
     def self.notify(notification)
-      notifiable = notification.notifiee.person.phone_numbers.find_by_confirmed(true)
-      
       host          = ENV['SMTP_HOST'] || JoyentConfig.smtp_host
       user          = ENV['SMTP_USER'] || nil
       pass          = ENV['SMTP_PASS'] || nil
       auth          = ENV['SMTP_AUTH'] || nil
-      
-      body =<<EOS
-#{notification.notifier.full_name} notified you about #{notification.item.name} (#{notification.item.class_humanize}).
-
-#{MessageHelper.url_for(notification.item)}
-EOS
-
+    
       tmail         = TMail::Mail.new
       tmail.from    = notification.notifier.system_email
-      tmail.body    = body
-      tmail.to      = notifiable.sms_address
+      tmail.body    = "#{MessageHelper.url_for(notification.item)} #{notification.notifier.full_name} notified you about #{notification.item.name} (#{notification.item.class_humanize})."
       tmail.subject = "Notification: #{notification.item.name}"
 
-      unless ENV['RAILS_ENV'] == 'test' or notifiable.blank?
-        Net::SMTP.start(host, 25, host, user, pass, auth) do |smtp|
-          smtp.send_message tmail.encoded, tmail.from, tmail.destinations
+      unless ENV['RAILS_ENV'] == 'test' or recipients(notification).empty?
+        recipients(notification).each do |sms_recipient|
+          tmail.to = sms_recipient.sms_address
+          Net::SMTP.start(host, 25, host, user, pass, auth) do |smtp|
+            smtp.send_message tmail.encoded, tmail.from, tmail.destinations
+          end
         end
       end
     end
+    
+    def self.recipients(notification)
+      notification.notifiee.person.phone_numbers.select{|ph| ph.use_notifier? && ph.confirmed?}
+    end
+    
   end
 end
