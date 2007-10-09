059588b36172271bfc787a88fae31bd4bb67f6a4
chris@joyent.com <chris@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2007-10-09 16:53:28 +0000

    adding new license to files in app.
    git-svn-id: http://svn.joyent.com/opensource/connector/source/trunk@446 7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a

--- app/helpers/corel_api_helper.rb
+++ app/helpers/corel_api_helper.rb
@@ -1,2 +1,14 @@
+=begin #(fold)
+++
+Copyright 2004-2007 Joyent Inc.
+
+Redistribution and/or modification of this code is 
+governed by the GPLv2.
+
+Report issues and contribute at http://dev.joyent.com/
+
+$Id$
+--
+=end #(end)
 module CorelApiHelper
 end
