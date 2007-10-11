469c2ba64eac9f6c4cf871c4115fa77635bcecc8
luke@joyent.com <luke@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2007-10-11 21:52:07 +0000

    - license new files
    git-svn-id: http://svn.joyent.com/opensource/connector/source/trunk@476 7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a

--- db/migrate/165_add_forwarding_column.rb
+++ db/migrate/165_add_forwarding_column.rb
@@ -1,3 +1,16 @@
+=begin #(fold)
+++
+Copyright 2004-2007 Joyent Inc.
+
+Redistribution and/or modification of this code is 
+governed by the GPLv2.
+
+Report issues and contribute at http://dev.joyent.com/
+
+$Id$
+--
+=end #(end)
+
 class AddForwardingColumn < ActiveRecord::Migration
   def self.up
     add_column "users", "forward_address", :string, :default => ''
--- test/unit/mail_alias_membership_test.rb
+++ test/unit/mail_alias_membership_test.rb
@@ -1,3 +1,16 @@
+=begin #(fold)
+++
+Copyright 2004-2007 Joyent Inc.
+
+Redistribution and/or modification of this code is 
+governed by the GPLv2.
+
+Report issues and contribute at http://dev.joyent.com/
+
+$Id$
+--
+=end #(end)
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MailAliasMembershipTest < Test::Unit::TestCase
